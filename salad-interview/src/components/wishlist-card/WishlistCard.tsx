68552a7adefb84f2f1efce882a99ada234fbace9
daitkenbacon <104740593+daitkenbacon@users.noreply.github.com>
2022-11-23 11:48:28 -0600

    Prettier

--- salad-interview/src/components/wishlist-card/WishlistCard.tsx
+++ salad-interview/src/components/wishlist-card/WishlistCard.tsx
@@ -13,31 +13,31 @@ type WishlistCardProps = {
 
 export default function WishlistCard(props: WishlistCardProps) {
   return (
-      <motion.div
-        initial={{ opacity: 0, y: 25 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        whileHover={{
-          rotate: 1,
-          transition: { duration: .2 },
-        }}
-        whileTap={{
-          scale: 1.01,
-          transition: { duration: .1 }
-        }}
-      >
-        <div
-          key={props.id}
-          className={`card-container 
+    <motion.div
+      initial={{ opacity: 0, y: 25 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      whileHover={{
+        rotate: 1,
+        transition: { duration: 0.2 },
+      }}
+      whileTap={{
+        scale: 1.01,
+        transition: { duration: 0.1 },
+      }}
+    >
+      <div
+        key={props.id}
+        className={`card-container 
                 ${props.selected ? "selected" : ""}
                 `}
-        >
-          <img src={props.background_image} alt={props.name} />
-          <div className="card-footer">
-            <p className="title">{props.name}</p>
-            <p className="released">{props.released}</p>
-            {props.variant === "skeleton" && <div className="loader"></div>}
-          </div>
+      >
+        <img src={props.background_image} alt={props.name} />
+        <div className="card-footer">
+          <p className="title">{props.name}</p>
+          <p className="released">{props.released}</p>
+          {props.variant === "skeleton" && <div className="loader"></div>}
         </div>
-      </motion.div>
+      </div>
+    </motion.div>
   );
 }
